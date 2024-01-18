73a076a29a2950278f9acde87a12925920463149
Sunyoung Lee <71091090+godltjsdud@users.noreply.github.com>
2024-01-19 08:20:28 +0900

    Update 이선영_쇠막대기.java

--- 1주차_자료구조/BOJ_10799/이선영_쇠막대기.java
+++ 1주차_자료구조/BOJ_10799/이선영_쇠막대기.java
@@ -1,1 +1,30 @@
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ_10799 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Character> stack = new Stack<Character>();
+		String input = br.readLine();
+		int result = 0;
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (c == '(') {
+				stack.push(c);
+			} else {
+				stack.pop();
+				if(input.charAt(i-1) == '(')
+					result += stack.size();
+				else {
+					result++;
+				}
+			}
+		}
+
+		System.out.println(result);
+	}
+
+}
