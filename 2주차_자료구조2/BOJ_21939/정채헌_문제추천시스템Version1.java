71ea469a24ee2904cb9d705639416152820cb7f3
chaeheonjeong <116990111+chaeheonjeong@users.noreply.github.com>
2024-01-29 20:50:51 +0900

    Update 정채헌_문제추천시스템Version1.java

--- 2주차_자료구조2/BOJ_21939/정채헌_문제추천시스템Version1.java
+++ 2주차_자료구조2/BOJ_21939/정채헌_문제추천시스템Version1.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 문제추천시스템Version1 {
-    public static void main(String[] args) throws IOException {
+public class Main {
+    public static void 문제추천시스템Version1(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         TreeMap<Integer, TreeSet<Integer>> map = new TreeMap<>();
 
@@ -45,7 +45,10 @@ public class 문제추천시스템Version1 {
                         toRemove.add(difficulty);
                     }
                 });
-                toRemove.forEach(map::remove);
+                for (Integer difficulty : toRemove) {
+                    map.remove(difficulty);
+                }
+
             }
         }
     }
